f360e7a877539fbd563016e9bdd18c8f999c7629
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2024-06-11 16:36:59 -0300

    Possibilidade de quatidade de itens

--- JuvTech/exercicios-Classes-Objetos/Execicios/exercicio02/scripts.js
+++ JuvTech/exercicios-Classes-Objetos/Execicios/exercicio02/scripts.js
@@ -5,20 +5,35 @@ class Carrinho {
           this.valorTotal = 0;
      }
 
-     adicionarItens(nome,valor) {
-          this.itens.push({nome,valor});
-          this.quantidadeTotal += 1;
-          this.valorTotal += valor;
+     adicionarItens(nome,quantidade=1,valor=0) {
+          let encontrouItem = false;
+          for (let item of this.itens) {
+               if (item.nome == nome) {
+                    item.quantidade += quantidade;
+                    this.quantidadeTotal+=quantidade;
+                    this.valorTotal += quantidade*item.valor
+                    encontrouItem = true;
+               }
+          }
+          if (!encontrouItem) {
+               this.itens.push({nome,quantidade,valor});
+               this.quantidadeTotal += quantidade;
+               this.valorTotal += quantidade*valor;
+          }
      }
 
      removerItens(nome) {
           this.itens.forEach(item => {
                if (item.nome == nome) {
                     let i = this.itens.indexOf(item);
-                    
+
+                    item.quantidade -= 1
                     this.quantidadeTotal -= 1;
                     this.valorTotal -= item.valor
-                    this.itens.splice(i,1);
+
+                    if (item.quantidade == 0 ) {
+                         this.itens.splice(i,1);
+                    }
                }
           });
      }
@@ -30,13 +45,13 @@ console.log(...c1.itens);
 console.log(c1.quantidadeTotal);
 console.log(c1.valorTotal);
 
-c1.adicionarItens("Nescau",20)
+c1.adicionarItens("Nescau",1,20)
 console.log(...c1.itens);
 
-c1.adicionarItens("Leite",12)
+c1.adicionarItens("Leite",1,12)
 console.log(...c1.itens);
 
-c1.adicionarItens("max steel",25)
+c1.adicionarItens("max steel",1,25)
 console.log(...c1.itens);
 
 c1.removerItens("max steel")

16c02ffe6d75684ee620246f3b236b9623deeda3
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2024-06-13 16:46:06 -0300

    Exercicios 1 e 2 resolvidos

---
+++ JuvTech/Expressões_Regulares/exercicio01/scripts.js
@@ -0,0 +1,4 @@
+const validarLetraMaiuscula = /[A-Z]/
+
+console.log(validarLetraMaiuscula.test("Ramona"));
+console.log(validarLetraMaiuscula.test("ramona"));
---
+++ JuvTech/Expressões_Regulares/exercicio02/scripts.js
@@ -0,0 +1,4 @@
+const validarTerminarID = /([a-zA-Z]+)?ID/
+
+console.log(validarTerminarID.test("asfsfID"));
+console.log(validarTerminarID.test("ID"));

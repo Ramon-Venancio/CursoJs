2d7752dd021303e76ef1e834981274206c9d43f5
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2024-06-26 16:48:54 -0300

    Centralização

--- JuvTech/Programacao_Assincrona/exerciocio05/scripts.js
+++ JuvTech/Programacao_Assincrona/exerciocio05/scripts.js
@@ -1,1 +1,9 @@
 // Crie um jogo da memória assíncrono utilizando JavaScript e HTML. O jogo deve carregar as imagens das cartas de forma assíncrona e revelar as cartas quando clicadas. Utilize setTimeout para simular o tempo de espera entre a revelação das cartas e async/await para gerenciar o fluxo assíncrono do jogo.
+
+const images = document.querySelectorAll('img');
+
+console.log(images)
+
+for (const image of images) {
+     console.log(image)
+}

3bc50bbf562c004a91406afee31c608f0b9a8862
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2024-10-03 16:37:35 -0300

    Terminação dos mini projetos e inicio da calculadora

---
+++ Curso-Web/Vue/vue/calculator/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+     <h1>Olá Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+     @font-face {
+          font-family: "RobotoMono";
+          src: url(./fonts/RobotoMono-VariableFont_wght.ttf);
+     }
+
+     * {
+          font-family: "RobotoMono", monospace;
+     }
+</style>

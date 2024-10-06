60e2c3f86ec9fe10a4c376788c3a43dbe5d19948
Ramon Venancio <ramonvenanciosousa@gmail.com>
2024-10-06 17:02:34 -0300

    Projeto Calculadora Vue finalizado

--- Curso-Web/Vue/vue/calculator/src/App.vue
+++ Curso-Web/Vue/vue/calculator/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
      <h1>Olá Vue!</h1>
+     <Calculator />
   </div>
 </template>
 
 <script>
+import Calculator from "./main/AppCalculator.vue"
 export default {
-
+     components: { Calculator }
 }
 </script>
 
@@ -18,5 +20,21 @@ export default {
 
      * {
           font-family: "RobotoMono", monospace;
+     }
+
+     body{
+          margin: 0;
+     }
+
+     #app {
+          display: flex;
+          flex-direction: column;
+          height: 100vh;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
+
+          color: #fff;
+          background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
      }
 </style>
